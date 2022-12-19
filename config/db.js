2a0bb9cf77f2d89ecc41ecfa00c1b42f3a4b2987
KarthikAlakunta <108953557+KarthikAlakunta@users.noreply.github.com>
2022-12-19 17:01:06 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose  = require('mongoose');
 
 const connect  =  async()=>{
     // return mongoose.connect("mongodb+srv://mongo:mongo@cluster0.wgamhn8.mongodb.net/products?retryWrites=true&w=majority");
-    return mongoose.connect("mongodb://127.0.0.1:27017/myntra");
+    return mongoose.connect("mongodb+srv://mongo:mongo@cluster0.wgamhn8.mongodb.net/?retryWrites=true&w=majority");
 }
 
 module.exports = connect;
